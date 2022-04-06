bb50922d620fa43ff90cb84d4ffae3b296c6ebb4
deminetzer <93319598+deminetzer@users.noreply.github.com>
2022-04-06 16:52:13 +0300

    Add files via upload

---
+++ food/EFoodType.java
@@ -0,0 +1,12 @@
+package food;
+
+/**
+ * An enum file describing the existing food types
+ * 
+ * @version April 2022
+ * @author Demi Netzer
+ *
+ */
+public enum EFoodType {
+	MEAT, NOTFOOD, VEGETABLE;
+}
